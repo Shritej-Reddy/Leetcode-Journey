c22682bb254c7b0e97edb625259f38021494eee7
= <=>
2024-09-22 22:26:29 +0530

    35. Search Insert Position

---
+++ 35. Search Insert Position/optimalSolution.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function(nums, target) {
+    // nums = [1,3,5,6]
+    // target = 5
+    let left = 0;
+    let right = nums.length - 1;
+
+    while(left <= right) {
+        let middle = Math.floor((left + right) / 2);
+
+        if(nums[middle] == target){
+            return middle;
+        }
+        else if(nums[middle] > target) {
+            right = middle - 1;
+        }
+        else {
+            left = middle + 1;
+        }
+    }
+    return left;
+};
