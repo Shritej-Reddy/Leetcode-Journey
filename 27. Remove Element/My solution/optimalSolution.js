5f3692068fa987df313931b705834144e8c154d1
= <=>
2024-09-22 22:04:33 +0530

    27

--- 27. Remove Element/My solution/optimalSolution.js
+++ 27. Remove Element/My solution/optimalSolution.js
@@ -4,17 +4,12 @@
  * @return {number}
  */
 var removeElement = function(nums, val) {
-    // nums = [3, 2, 2, 3]
-    // val = 3
     let k = 0;
     for(let i = 0; i < nums.length; i++) {
-        // console.log(nums[i], " | ", i)
         if(nums[i] != val) {
-            // console.log(nums[i], " - ", i)
             nums[k] = nums[i]
             k += 1;
         }
-        // console.log("k - ", k);
     }
     return k;
 };
